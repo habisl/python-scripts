b70b15322d8e9cfc9127746bb7f99f6656f43186
Habibul Islam <habibulislam72@yahoo.com>
2018-02-01 01:44:29 +0200

    Update app1.py

--- Interactive Python Dictionary/app1.py
+++ Interactive Python Dictionary/app1.py
@@ -4,21 +4,19 @@
 # If doesnt exist ask for new word
 import json
 from difflib import get_close_matches  
-# This module provides classes and functions for comparing sequences. It can be used for example, for comparing files, 
-# and can produce difference information in various formats, including HTML and context and unified diffs.
 
 data = json.load(open("data.json"))
 
 
 def translate(w):
-    w = w.lower() # .lower() was used to get all the lowercase matching from json
+    w = w.lower() 
     if w in data:
         return data[w]
-    elif w.title() in data:  # if user entered "texas" this will check for "Texas" as well.
+    elif w.title() in data:  
         return data[w.title()]
     elif w.upper() in data: #in case user enters words like USA or NATO
         return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0: # so when the word is more than 1 or letter difflib module will provide suggestion
+    elif len(get_close_matches(w, data.keys())) > 0: #word is more than 1 or letter difflib module will provide suggestion
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
