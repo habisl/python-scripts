8e969ee443e6ba71296be7d0ab927b22ff3319fb
Habibul Islam <22364354+habisl@users.noreply.github.com>
2018-02-15 23:41:14 +0200

    Update site_checker.py

--- Website Checker/SiteChecker/site_checker.py
+++ Website Checker/SiteChecker/site_checker.py
@@ -8,23 +8,16 @@ __version__ = 3.0
 
 import json
 
-
 json_file = './config.json'
 file_name = './log_report.txt'
-
 
 with open(json_file) as config_file:
     config = json.load(config_file)
 print(json.dumps(config, indent=4))
 
-
 log_file = open(file_name, "w")
 log_file.write("Name" + "\t" + "   Date" + "\t" + "\t" + " Time" + "\t" + "Status"
-                        + "\t" + "  Ping Time(ms)" + "  " + "Report" + "\n")  # Logs header
-                      
-                     
-                      
-
+               + "\t" + "  Ping Time(ms)" + "  " + "Report" + "\n")  # Logs header
 
 for i in range(0, int(config['frequency']['total_itarations'])):  # number of times it should check
     time.sleep(int(config['frequency']["check_interval"]))  # In seconds
@@ -36,6 +29,13 @@ for i in range(0, int(config['frequency']['total_itarations'])):  # number of ti
             start = time.time()
             soup = BeautifulSoup(req.text, 'html.parser')
             content = soup.findAll(text=site['content'])
-# Need to be implemented soon... 
-log_file.close()
-# Need to be implemented soon... 
+            print(site_name)
+            print(req)
+            print(content)
+        except HTTPError as e:
+            status = 'Not OK'
+        else:
+            print(OK)
+        # Need to be implemented soon... 
+        log_file.close()
+        # Need to be implemented soon... 
