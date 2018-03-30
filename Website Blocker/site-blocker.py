35bf1baf80bb14981f406bd6cd27994378239e06
Habibul Islam <habibulislam72@yahoo.com>
2018-03-30 13:04:47 +0300

    Update site-blocker.py

--- Website Blocker/site-blocker.py
+++ Website Blocker/site-blocker.py
@@ -1,7 +1,19 @@
-
+import time
+from datetime import datetime as dt
 
 hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+   
